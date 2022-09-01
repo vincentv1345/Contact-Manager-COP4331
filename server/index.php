82544003fb477033604863a2a4b7c3d47bb605bf
vincentv1345 <vincentverapen2012@gmail.com>
2022-09-01 13:44:45 -0400

    Added contacts and user routes

--- server/index.php
+++ server/index.php
@@ -5,7 +5,22 @@
     
         //validate the correct routs for this api
         if (isset($uri[1]) && $uri[1] === "index.php" && isset($uri[2])){
-    
+
+                //useers route
+                if($uri[2] === "users"){
+                    echo "This is the users end point";
+                }
+        
+                //contact route
+                else if($uri[2] === "contacts"){
+                    echo "This is the contacts end point";
+                }
+                
+                //throw error for invalid routes
+                else{
+                    header("HTTP/1.1 404 Not Found");
+                    echo "HTTP/1.1 404 Not Found";
+                }
         }
 
         //throw an 404 error the routes are not valid
