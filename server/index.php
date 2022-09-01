3a015244815de382a25766eaa4c9d512a0623497
Joseph <josephmt1176@gmail.com>
2022-09-01 13:37:00 -0400

    added routes and validation added users and contacts routes and validation. also comments

--- server/index.php
+++ server/index.php
@@ -1,5 +1,16 @@
 <?php
+        //This gets the uri routes into an array
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $uri = explode( '/', $uri );
-        print_r($uri);
+    
+        //validate the correct routs for this api
+        if (isset($uri[1]) && $uri[1] === "index.php" && isset($uri[2])){
+    
+        }
+
+        //throw an 404 error the routes are not valid
+        else{
+            header("HTTP/1.1 404 Not Found");
+            echo "HTTP/1.1 404 Not Found";
+        }
 ?>
