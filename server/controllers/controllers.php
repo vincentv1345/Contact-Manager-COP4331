111e1b24b102d8ddb36dedbe5ea0474e438260f8
Joseph <josephmt1176@gmail.com>
2022-09-08 13:55:08 -0400

    testing getAll controller

--- server/controllers/controllers.php
+++ server/controllers/controllers.php
@@ -4,6 +4,8 @@
 
         //Validate id from request body
         print_r($body);
+        print_r($body['id']);
+        print_r($body['id']);
         if(isset($body['id']) && is_numeric($body['id'])){
             
             $DBquery = "Select * from ". $route . " where userID = " . $body['id'] . ";";
