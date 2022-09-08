77ca8b29054c76d10ce71f51fcd33eb525000fa8
Joseph <josephmt1176@gmail.com>
2022-09-08 14:06:52 -0400

    testing getAll controller

--- server/controllers/controllers.php
+++ server/controllers/controllers.php
@@ -3,15 +3,22 @@
     function getAll(string $route, string $body, string $query, $db) {
 
         //Validate id from request body
+        echo "1";
         if(isset($body["id"]) && is_numeric($body["id"])){
-            
+            echo "2";
             $DBquery = "Select * from ". $route . " where userID = " . $body["id"] . ";";
+            echo "3";
             $result = mysqli_query($db, $DBquery);
+            echo "4";
+
             $response = array();
+            echo "5";
 
             if (mysqli_num_rows($result) > 0) {
+                echo "6";
 
                 while($row = mysqli_fetch_assoc($result)) {
+                    echo "7";
                     array_push($response, "FirstName: " . $row["FirstName"] . ", 
                                         LastName: " . $row["LastName"] . ",
                                         Email: " . $row["Email"] . ", 
@@ -19,6 +26,7 @@
                                         Address: " . $row["Address"] . ", 
                                         Status: " . $row["Status"]);
                 }
+                echo "8";
                 echo json_encode($response);
             } 
             else {
