e04eaf0e21ed3792632e2a2b90f5f267a4b51875
santiagoposse <55883946+santiagoposse@users.noreply.github.com>
2022-09-12 17:30:15 -0400

    Delete server/frontend/add directory

--- server/frontend/add/src/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const createContactForm = document.querySelector("#createContact");
-
-    document.querySelector("#createContact").addEventListener("click", e => {
-        e.preventDefault();
-    });
-});
