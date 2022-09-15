fd0bb595410518a349c90bc0167627184a216d92
santiagoposse <55883946+santiagoposse@users.noreply.github.com>
2022-09-15 16:45:47 -0400

    Partially added error message

--- js/login.js
+++ js/login.js
@@ -1,22 +1,23 @@
-
 const submit = document.getElementById("submit");
 submit.addEventListener("click", (e) => {
 
     e.preventDefault();
 
-    const username = document.getElementById("user-login").value;
-    const password = document.getElementById("password").value;
-
-    fetch(`http://159.223.173.36/api/index.php/users?Login=${username}&Password=${password}`, {
+    const username = document.getElementById("user-login");
+    const password = document.getElementById("password");
+    
+    fetch(`http://159.223.173.36/api/index.php/users?Login=${username.value}&Password=${password.value}`, {
     })
-      .then((response) => response.json())
-      .then((data) => {
-        if(data.length > 0){
-          user = data[0];
-          window.location.replace("http://contactsplus.xyz/homePage.html");
-        }
+    .then((response) => response.json())
+    .then((data) => {
+      if(data.length > 0){
+        user = data[0];
+        window.location.replace("http://contactsplus.xyz/homePage.html");
+      }
         else{
           console.log("WRONG CREDENTIALS")
+          username.className = "relative block w-full appearance-none rounded-none rounded-t-md border-[0.1rem] border-solid border-red-600 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm";
+          password.className = "relative block w-full appearance-none rounded-none rounded-b-md border-[0.1rem] border-solid border-red-600 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm";
         }
         
       })
