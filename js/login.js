1948c05fec1a672111e2eba49a474acd6e50ead5
santiagoposse <55883946+santiagoposse@users.noreply.github.com>
2022-09-16 17:19:54 -0400

    fixed error message for incorrect input

--- js/login.js
+++ js/login.js
@@ -5,6 +5,7 @@ submit.addEventListener("click", (e) => {
 
     const username = document.getElementById("user-login");
     const password = document.getElementById("password");
+    const error = document.getElementById("error");
     
     fetch(`http://159.223.173.36/api/index.php/users?Login=${username.value}&Password=${password.value}`, {
     })
@@ -16,6 +17,8 @@ submit.addEventListener("click", (e) => {
       }
         else{
           console.log("WRONG CREDENTIALS")
+          error.className = "text-sm text-center text-red-600 font-semibold" 
+          error.textContent= "Incorrect Username or Password";
           username.className = "relative block w-full appearance-none rounded-none rounded-t-md border-[0.1rem] border-solid border-red-600 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm";
           password.className = "relative block w-full appearance-none rounded-none rounded-b-md border-[0.1rem] border-solid border-red-600 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm";
         }
